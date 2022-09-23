5588745e1e1591ea6b9002d9293517c7133809b4
George <caldarescu.george@gmail.com>
2022-09-23 16:21:43 +0200

    create dropdown menu

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
+// dropdown menu
 
-navToggle.addEventListener("click", () => {
-  nav.classList.toggle("nav--visible");
+document.addEventListener("click", (e) => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]");
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return;
+  // the code above ignore the cliks inside the dropdown menus
+
+  let currentDropdown;
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
+  }
+
+  // close the other dropdowns that are not open
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
 });
